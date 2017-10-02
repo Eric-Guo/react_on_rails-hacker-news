3598897bb100f4a1eef6047e14ccf1792899e5b1
Eric-Guo <guochunzhong@bayekeji.com>
2017-10-02 18:08:29 +0800

    Remove not necessary layout

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class HelloWorldController < ApplicationController
-  layout "hello_world"
-
   def index
     @hello_world_props = { name: "Stranger" }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'hello_world#index'
 end
