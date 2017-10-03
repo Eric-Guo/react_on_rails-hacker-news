51551d2144cdef965bbcbaef05c802f1bcaa4e23
Eric-Guo <eric@managebac.com>
2017-10-03 11:49:09 +0800

    Rename HelloWord to home controller and App component.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class HelloWorldController < ApplicationController
+class HomeController < ApplicationController
   def index
     @hello_world_props = { name: "Stranger" }
   end
---
+++ app/javascript/packs/app-bundle.js
@@ -0,0 +1,7 @@
+import ReactOnRails from 'react-on-rails';
+
+import App from '../bundles/Home/components/App';
+
+ReactOnRails.register({
+  App,
+});
